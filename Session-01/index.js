b56cfdc4cc9e7e628eb7e3982585827d5ede722f
Adolfo Gomez <agomezligero@yahoo.es>
2022-10-03 18:12:56 +0200

    first commit

---
+++ Session-01/index.js
@@ -0,0 +1,8 @@
+//console.log("Esto es una prueba de impresión");
+
+console.log("Hola");
+console.log("Adios");
+
+var saludo = prompt("¿Cómo quiere saludar?");
+
+console.log("El usuario ha saludado con: ", saludo);
